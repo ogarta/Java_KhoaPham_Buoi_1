6268b07a5e55d54e16b36848ba45d6812128bb7c
ogata-higan <lviet456@gmail.com>
2022-04-02 19:29:09 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Le Phan Van Viet";
+        int age = 20;
+        double he = 1.5;
+        String favorite = "manga";
+        long phong = 282349463;
+        String work = "student";
+
+    }
+}
